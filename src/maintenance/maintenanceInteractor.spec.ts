e6751ffa77a69b77514d28e82e2603862b41a782
Paige Zaleppa <pz0342@gmail.com>
2019-09-20 10:18:32 -0400

    :sparkles: Add testing

--- src/maintenance/maintenanceInteractor.spec.ts
+++ src/maintenance/maintenanceInteractor.spec.ts
@@ -1,1 +1,13 @@
+import * as maintenance from './maintenanceInteractor';
+import { }
+
+describe('get maintenance status', () => {
+    it('should return a boolean to determine if the site is under maintenance', async () => {
+        const clarkDown = await maintenance.getMaintenanceStatus();
+        expect(clarkDown).resolves.toBeDefined();
+    });
+});
+
+
+
 
