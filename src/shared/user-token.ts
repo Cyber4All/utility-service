d4de529ece354331524772e507394c71d861389f
Paige Zaleppa <pz0342@gmail.com>
2019-09-26 13:58:09 -0400

    :sparkles: Add user token

---
+++ src/shared/user-token.ts
@@ -0,0 +1,20 @@
+/**
+ * The identity of a user in the CLARK system that allows us to authorize requests.
+ * @interface
+ */
+export interface UserToken {
+    username: string;
+    name: string;
+    email: string;
+    organization: string;
+    emailVerified: boolean;
+    accessGroups: string[];
+}
+export const accessGroups = {
+    ADMIN: 'admin',
+    EDITOR: 'editor',
+    CURATOR: 'curator',
+    REVIEWER: 'reviewer',
+    USER: 'user',
+};
+
